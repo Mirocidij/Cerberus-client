62602667b8d63d52ff93130ff5fd746c3bc70749
Yatsutko Sergey <s3rzh@inbox.ru>
2021-04-18 13:34:35 -0400

    Clean defaults

---
+++ src/Features/Home/App.js
@@ -0,0 +1,14 @@
+import './css/App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <header className="Home-header">
+
+                </header>
+            </div>
+        );
+    }
+}
